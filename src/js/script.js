8729b8f59cd50ce50bf3f74f4e4e8520f7679a64
Krystian Kwiatos <matius4321@gmail.com>
2021-11-09 15:11:54 +0100

    zadanie nr 1

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       
       console.log('new Product:', thisProduct);
@@ -84,6 +85,15 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+    }
+    initAmountWidget(){
+      const thisProduct = this;
+
+      thisProduct.AmountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', () => {
+        thisProduct.processOrder();
+      });
     }
     initAccordion(){
       const thisProduct = this;
@@ -177,8 +187,63 @@
           }
         }
       }
-      // update calculated price in the HTML
+      //nie mam pojęcia czemu po dodaniu tego wyrzuca mi błąd cannot read value :<
+      //price *= thisProduct.amountWidget.value;
       thisProduct.priceElem.innerHTML = price;
+    }
+  }
+
+  class AmountWidget{
+    constructor(element){
+      
+      const thisWidget = this;
+      thisWidget.getElements(element);
+      thisWidget.setValue(settings.amountWidget.defaultValue);
+    }
+
+    getElements(element){
+      const thisWidget = this;
+      
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value){
+      const thisWidget = this;
+      const newValue = parseInt(value);
+
+      if(thisWidget.value !== newValue && !isNaN(newValue)) {
+        thisWidget.value = newValue &&
+        newValue >= parseInt(settings.amountWidget.defaultMin) &&
+        newValue <= parseInt(settings.amountWidget.defaultMax);
+      }
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+      thisWidget.announce();
+    }
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', (event) => {
+        event.preventDefault();
+        this.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.setValue(thisWidget.value - 1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.setValue(thisWidget.value + 1);
+      });
+    }
+    announce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
 
