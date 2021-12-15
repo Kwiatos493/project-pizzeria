8a7a4f3d3c56be53e053c3b2e8a5d87e988ea90d
Krystian Kwiatos <matius4321@gmail.com>
2021-12-15 13:26:40 +0100

    pizzeria 10.4 60%

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,38 @@ const app = {
   initPages: function(){
     const thisApp = this;
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    
-    thisApp.activatePage(thisApp.pages[0].id);
 
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = false;  
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click',function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change url hash */
+        window.location.hash = '#/' + id;
+      });
+    }
   },
 
   activatePage: function(pageId){
